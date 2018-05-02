18b079ce0f459f109c70f8555163b2e430fbd259
Matthew0123 <634629769@qq.com>
2018-05-02 16:59:34 +0800

    微信小程序获取openId接口调整

--- swan-express/src/main/java/com/hitrobotgroup/hiiri/swan/express/ExpressServiceApplication.java
+++ swan-express/src/main/java/com/hitrobotgroup/hiiri/swan/express/ExpressServiceApplication.java
@@ -2,6 +2,10 @@ package com.hitrobotgroup.hiiri.swan.express;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /*
  * Copyright (C) 2016-2017 HIIRI Inc.All Rights Reserved. 
@@ -16,6 +20,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class ExpressServiceApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/v1/*/*").allowedOrigins("*");
+                registry.addMapping("/v1/*/*/*").allowedOrigins("*");
+            }
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ExpressServiceApplication.class, args);
     }
--- swan-express/src/main/java/com/hitrobotgroup/hiiri/swan/express/control/WeixinApiControl.java
+++ swan-express/src/main/java/com/hitrobotgroup/hiiri/swan/express/control/WeixinApiControl.java
@@ -1,10 +1,10 @@
 /**
  * CopyRight (C) 2016-2017 HIIRI Inc.All Rights Reserved.
- * <p>
+ *
  * FileName  swan
- * <p>
+ *
  * Description
- * <p>
+ *
  * History:
  * Version     Author                 Date
  * 1.0         fanmx            2018/3/27  15:25
@@ -13,6 +13,9 @@ package com.hitrobotgroup.hiiri.swan.express.control;
 
 import com.alibaba.fastjson.JSONObject;
 import com.hitrobotgroup.hiiri.swan.express.pojo.JsonResult;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -21,25 +24,28 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
 @RestController
 @RequestMapping("/v1/api/weixin")
+@EnableScheduling
 public class WeixinApiControl {
 
-    private static String url = "https://api.weixin.qq.com/sns/jscode2session";
-    private static String appId = "wx5d35120aeca69f75";
-    private static String appSecret = "775042d74503c4c8bbdc2024320b953b";
-    private static Logger logger = LoggerFactory.getLogger(WeixinApiControl.class);
+    //公众平台接口通用域名
+    private static String url="https://api.weixin.qq.com/";
+    //微信小程序
+    private static String appId="wx5d35120aeca69f75";
+    private static String appSecret="775042d74503c4c8bbdc2024320b953b";
+
+    private static Logger logger= LoggerFactory.getLogger(WeixinApiControl.class);
     private static HttpClient client = HttpClientBuilder.create().build();
 
-    public static JSONObject invokeService(String queryCase) {
-        HttpPost request = new HttpPost(url);
+
+    private HttpEntity getHttp(String method,String queryCase) throws IOException {
+        HttpPost request = new HttpPost(method);
         logger.info(queryCase);
         StringEntity reqEntity = null;
         try {
@@ -49,10 +55,20 @@ public class WeixinApiControl {
         }
         reqEntity.setContentType("application/x-www-form-urlencoded");
         request.setEntity(reqEntity);
-        JSONObject jsonResult = null;
+        HttpResponse response = client.execute(request);
+        return  response.getEntity();
+    }
+
+    /**
+     *
+     * @param method
+     * @param queryCase
+     * @return
+     */
+    private JSONObject invokeService(String method,String queryCase) {
+        JSONObject jsonResult=null;
         try {
-            HttpResponse response = client.execute(request);
-            String strResult = EntityUtils.toString(response.getEntity());
+            String strResult = EntityUtils.toString(getHttp(method,queryCase),"utf-8");
             jsonResult = JSONObject.parseObject(strResult);
             logger.info("result:" + jsonResult);
 
@@ -62,12 +78,16 @@ public class WeixinApiControl {
         return jsonResult;
     }
 
-
+    /**
+     * 微信小程序获取openid
+     * @param code
+     * @return
+     */
     @RequestMapping(value = "getLoginMsg", method = {RequestMethod.POST, RequestMethod.GET})
     JsonResult getLoginMsg(String code) {
         try {
             String queryCase = "appid=" + appId + "&secret=" + appSecret + "&grant_type=authorization_code&js_code=" + code;
-            JSONObject jsonObject = invokeService(queryCase);
+            JSONObject jsonObject=invokeService(url+"sns/jscode2session",queryCase);
             return new JsonResult(true, jsonObject);
         } catch (Exception ex) {
             return new JsonResult(false, ex.getMessage());
